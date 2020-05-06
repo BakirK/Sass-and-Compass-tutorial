1a7e68349340855cb09a3e014b01699935db21cd
Bakir Karovic <bakir.karovic@gmail.com>
2020-05-06 15:07:32 +0200

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).on('load', function () {
+    this.console.log("bababa");
+    $('.gallery').masonry({
+        itemSelector: '.gcontainer',
+        gutter: 15,
+        columnWidth: 230
+    });
+});
